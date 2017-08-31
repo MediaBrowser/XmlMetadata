5d08e17927e063d77f40628e012a08f95c00a0ed
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-08-31 01:04:55 -0400

    initial

---
+++ XmlMetadata/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Serialization;
+using XmlMetadata.Configuration;
+
+namespace XmlMetadata
+{
+    public class Plugin : BasePlugin<PluginConfiguration>
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
+        {
+        }
+
+        private Guid _id = new Guid("2850d40d-9c66-4525-aa46-968e8ef04e97");
+        public override Guid Id
+        {
+            get { return _id; }
+        }
+
+        /// <summary>
+        /// Gets the name of the plugin
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get { return "Xml Metadata"; }
+        }
+
+        public static string MetadataName
+        {
+            get { return "Emby Xml"; }
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public override string Description
+        {
+            get
+            {
+                return "Supports Emby Legacy Xml metadata";
+            }
+        }
+    }
+}
