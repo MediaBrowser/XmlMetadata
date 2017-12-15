e6e68f9f42f9257512cca151aeb5e36008fd2983
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-12-15 15:12:33 -0500

    add person classes

---
+++ XmlMetadata/Providers/PersonXmlProvider.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Threading;
+using MediaBrowser.Model.IO;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.IO;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Model.Xml;
+using XmlMetadata.Parsers;
+
+namespace XmlMetadata.Providers
+{
+    public class PersonXmlProvider : BaseXmlProvider<Person>
+    {
+        private readonly IProviderManager _providerManager;
+        protected IXmlReaderSettingsFactory XmlReaderSettingsFactory { get; private set; }
+
+        public PersonXmlProvider(IFileSystem fileSystem, ILogger logger, IProviderManager providerManager, IXmlReaderSettingsFactory xmlReaderSettingsFactory)
+            : base(fileSystem, logger)
+        {
+            _providerManager = providerManager;
+            XmlReaderSettingsFactory = xmlReaderSettingsFactory;
+        }
+
+        protected override void Fetch(MetadataResult<Person> result, string path, CancellationToken cancellationToken)
+        {
+            new BaseItemXmlParser<Person>(Logger, _providerManager, XmlReaderSettingsFactory, FileSystem).Fetch(result, path, cancellationToken);
+        }
+
+        protected override FileSystemMetadata GetXmlFile(ItemInfo info, IDirectoryService directoryService)
+        {
+            return directoryService.GetFile(Path.Combine(info.Path, "person.xml"));
+        }
+    }
+}
